7e8b5f6bf78f53f7c0dbde46aad114963c500867
Ryan Fabella <ryanthe@gmail.com>
2022-08-30 19:11:34 +0700

    merubah parameter pada login

--- login.php
+++ login.php
@@ -10,26 +10,28 @@ if (getenv("MERDEKAUN")==""){
     }else{
         $curl = curl_init();
         curl_setopt_array($curl, [
-            CURLOPT_URL => "https://api.kampusmerdeka.kemdikbud.go.id/v1alpha1/mentors/login",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
+           CURLOPT_URL => "https://api.kampusmerdeka.kemdikbud.go.id/v1alpha1/mentors/login",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+     
             CURLOPT_POSTFIELDS => "{\"email\":\"".getenv("MERDEKAUN")."\",\"password\":\"".getenv("MERDEKAPSS")."\"}",
             CURLOPT_HTTPHEADER => [
                 "Accept: application/json",
+                "Accept-Encoding: gzip, deflate, br",
                 "Accept-Language: en-GB,en-US;q=0.9,en;q=0.8",
                 "Cache-Control: no-cache",
                 "Connection: keep-alive",
-                "Content-Length: 63",
+                "Content-Length: 60",
                 "Content-Type: application/json",
                 "Host: api.kampusmerdeka.kemdikbud.go.id",
                 "Origin: https://mentor.kampusmerdeka.kemdikbud.go.id",
                 "Pragma: no-cache",
                 "Referer: https://mentor.kampusmerdeka.kemdikbud.go.id/",
-                "User-Agent: Mozilla/5.0 "
+                "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15"
             ],
         ]);
         $response = curl_exec($curl);
